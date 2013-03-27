ca42b27ce29c828e84a2d1f8f24a16af3b658d5d
Kai Wernicke <kai@4ware.net>
2013-03-27 11:03:24 +0100

    changed vpre to preset

--- lib/carrierwave/video/ffmpeg_options.rb
+++ lib/carrierwave/video/ffmpeg_options.rb
@@ -72,7 +72,7 @@ module CarrierWave
             when 'mp4'
               h[:video_codec] = 'libx264'
               h[:audio_codec] = 'libfaac'
-              h[:custom] = '-qscale 0 -preset slow -vpre baseline -g 30'
+              h[:custom] = '-qscale 0 -preset slow -preset baseline -g 30'
             when 'ogv'
               h[:video_codec] = 'libtheora'
               h[:audio_codec] = 'libvorbis'
