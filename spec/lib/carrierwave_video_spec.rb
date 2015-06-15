7a195092699d95a673bf9bc3cd31ebbed53849b6
Mark Campbell <nitrodist@gmail.com>
2015-06-15 15:44:53 -0400

    Use transpec to automatically convert rspec 2 specs to rspec 3
    Adds the 'raise_errors_for_deprecations' option to rspec so that we know
    when we're using deprecated rspec features.

--- spec/lib/carrierwave_video_spec.rb
+++ spec/lib/carrierwave_video_spec.rb
@@ -16,24 +16,24 @@ describe CarrierWave::Video do
 
   describe ".encode_video" do
     it "processes the model" do
-      TestVideoUploader.should_receive(:process).with(encode_video: ["format", :opts])
+      expect(TestVideoUploader).to receive(:process).with(encode_video: ["format", :opts])
       TestVideoUploader.encode_video("format", :opts)
     end
 
     it "does not require options" do
-      TestVideoUploader.should_receive(:process).with(encode_video: ["format", {}])
+      expect(TestVideoUploader).to receive(:process).with(encode_video: ["format", {}])
       TestVideoUploader.encode_video("format")
     end
   end
 
   describe ".encode_ogv" do
     it "processes the model" do
-      TestVideoUploader.should_receive(:process).with(encode_ogv: [:opts])
+      expect(TestVideoUploader).to receive(:process).with(encode_ogv: [:opts])
       TestVideoUploader.encode_ogv(:opts)
     end
 
     it "does not require options" do
-      TestVideoUploader.should_receive(:process).with(encode_ogv: [{}])
+      expect(TestVideoUploader).to receive(:process).with(encode_ogv: [{}])
       TestVideoUploader.encode_ogv
     end
   end
@@ -43,31 +43,31 @@ describe CarrierWave::Video do
     let(:movie) { double }
 
     before do
-      converter.stub(:current_path).and_return('video/path/file.mov')
+      allow(converter).to receive(:current_path).and_return('video/path/file.mov')
 
-      FFMPEG::Movie.should_receive(:new).and_return(movie)
+      expect(FFMPEG::Movie).to receive(:new).and_return(movie)
     end
 
     context "with no options set" do
-      before {  File.should_receive(:rename) }
+      before {  expect(File).to receive(:rename) }
 
       it "calls transcode with correct format options" do
-        movie.should_receive(:transcode) do |path, opts, codec_opts|
-          codec_opts.should == {preserve_aspect_ratio: :width}
+        expect(movie).to receive(:transcode) do |path, opts, codec_opts|
+          expect(codec_opts).to eq({preserve_aspect_ratio: :width})
 
-          opts[:video_codec].should == 'libvpx'
-          opts[:audio_codec].should == 'libvorbis'
-          opts[:custom].should == '-b 1500k -ab 160000 -f webm -g 30'
+          expect(opts[:video_codec]).to eq('libvpx')
+          expect(opts[:audio_codec]).to eq('libvorbis')
+          expect(opts[:custom]).to eq('-b 1500k -ab 160000 -f webm -g 30')
 
-          path.should == "video/path/tmpfile.#{format}"
+          expect(path).to eq("video/path/tmpfile.#{format}")
         end
 
         converter.encode_video(format)
       end
 
       it "provides a default for the resolution" do
-        movie.should_receive(:transcode) do |path, opts, codec_opts|
-          opts[:resolution].should == '640x360'
+        expect(movie).to receive(:transcode) do |path, opts, codec_opts|
+          expect(opts[:resolution]).to eq('640x360')
         end
 
         converter.encode_video(format)
@@ -75,7 +75,7 @@ describe CarrierWave::Video do
     end
 
     context "with callbacks set" do
-      before { movie.should_receive(:transcode) }
+      before { expect(movie).to receive(:transcode) }
       let(:opts) do
         { callbacks: {
              before_transcode: :method1,
@@ -87,13 +87,13 @@ describe CarrierWave::Video do
       end
 
       context "no exceptions raised" do
-        before {  File.should_receive(:rename) }
+        before {  expect(File).to receive(:rename) }
 
         it "calls before_transcode, after_transcode, and ensure" do
-          converter.model.should_receive(:method1).with(format, opts).ordered
-          converter.model.should_receive(:method2).with(format, opts).ordered
-          converter.model.should_not_receive(:method3)
-          converter.model.should_receive(:method4).with(format, opts).ordered
+          expect(converter.model).to receive(:method1).with(format, opts).ordered
+          expect(converter.model).to receive(:method2).with(format, opts).ordered
+          expect(converter.model).not_to receive(:method3)
+          expect(converter.model).to receive(:method4).with(format, opts).ordered
 
           converter.encode_video(format, opts)
         end
@@ -101,18 +101,18 @@ describe CarrierWave::Video do
 
       context "exception raised" do
         let(:e) { StandardError.new("test error") }
-        before { File.should_receive(:rename).and_raise(e) }
+        before { expect(File).to receive(:rename).and_raise(e) }
 
 
         it "calls before_transcode and ensure" do
-          converter.model.should_receive(:method1).with(format, opts).ordered
-          converter.model.should_not_receive(:method2)
-          converter.model.should_receive(:method3).with(format, opts).ordered
-          converter.model.should_receive(:method4).with(format, opts).ordered
+          expect(converter.model).to receive(:method1).with(format, opts).ordered
+          expect(converter.model).not_to receive(:method2)
+          expect(converter.model).to receive(:method3).with(format, opts).ordered
+          expect(converter.model).to receive(:method4).with(format, opts).ordered
 
-          lambda do
+          expect do
             converter.encode_video(format, opts)
-          end.should raise_exception(CarrierWave::ProcessingError)
+          end.to raise_exception(CarrierWave::ProcessingError)
         end
       end
     end
@@ -120,69 +120,69 @@ describe CarrierWave::Video do
     context "with logger set" do
       let(:logger) { double }
       before do
-        converter.model.stub(:logger).and_return(logger)
-        movie.should_receive(:transcode)
+        allow(converter.model).to receive(:logger).and_return(logger)
+        expect(movie).to receive(:transcode)
       end
 
       context "with no exceptions" do
-        before { File.should_receive(:rename) }
+        before { expect(File).to receive(:rename) }
 
         it "sets FFMPEG logger to logger and resets" do
           old_logger = ::FFMPEG.logger
-          ::FFMPEG.should_receive(:logger=).with(logger).ordered
-          ::FFMPEG.should_receive(:logger=).with(old_logger).ordered
+          expect(::FFMPEG).to receive(:logger=).with(logger).ordered
+          expect(::FFMPEG).to receive(:logger=).with(old_logger).ordered
           converter.encode_video(format, logger: :logger)
         end
       end
 
       context "with exceptions" do
         let(:e) { StandardError.new("test error") }
-        before { File.should_receive(:rename).and_raise(e) }
+        before { expect(File).to receive(:rename).and_raise(e) }
 
         it "logs exception" do
-          logger.should_receive(:error).with("#{e.class}: #{e.message}")
-          logger.should_receive(:error).any_number_of_times #backtrace
+          expect(logger).to receive(:error).with("#{e.class}: #{e.message}")
+          allow(logger).to receive(:error) #backtrace
 
-          lambda do
+          expect do
             converter.encode_video(format, logger: :logger)
-          end.should raise_exception(CarrierWave::ProcessingError)
+          end.to raise_exception(CarrierWave::ProcessingError)
         end
       end
     end
 
     context "with progress set" do
       before do
-        File.should_receive(:rename)
-        movie.stub(:transcode).and_yield(0.0).and_yield(1.0)
+        expect(File).to receive(:rename)
+        allow(movie).to receive(:transcode).and_yield(0.0).and_yield(1.0)
       end
       let(:opts) { {progress: :progress} }
 
       it "logs progress" do
-        converter.model.should_receive(:progress).with(0.0)
-        converter.model.should_receive(:progress).with(1.0)
+        expect(converter.model).to receive(:progress).with(0.0)
+        expect(converter.model).to receive(:progress).with(1.0)
         converter.encode_video(format, progress: :progress)
       end
 
       it "logs progress with format and options" do
-        converter.model.stub_chain(:method, :arity).and_return(3)
-        converter.model.should_receive(:progress).with(format, hash_including(opts), 0.0)
-        converter.model.should_receive(:progress).with(format, hash_including(opts), 1.0)
+        allow(converter.model).to receive_message_chain(:method, :arity).and_return(3)
+        expect(converter.model).to receive(:progress).with(format, hash_including(opts), 0.0)
+        expect(converter.model).to receive(:progress).with(format, hash_including(opts), 1.0)
         converter.encode_video(format, opts)
       end
     end
 
     context "with watermark set" do
-      before { File.should_receive(:rename) }
+      before { expect(File).to receive(:rename) }
 
       it "appends watermark params to custom params for ffmpeg" do
-        movie.should_receive(:transcode) do |path, opts, codec_opts|
-          codec_opts.should == {preserve_aspect_ratio: :width}
+        expect(movie).to receive(:transcode) do |path, opts, codec_opts|
+          expect(codec_opts).to eq({preserve_aspect_ratio: :width})
 
-          opts[:video_codec].should == 'libvpx'
-          opts[:audio_codec].should == 'libvorbis'
-          opts[:custom].should == "-b 1500k -ab 160000 -f webm -g 30 -vf \"movie=path/to/file.png [logo]; [in][logo] overlay=5:main_h-overlay_h-5 [out]\""
+          expect(opts[:video_codec]).to eq('libvpx')
+          expect(opts[:audio_codec]).to eq('libvorbis')
+          expect(opts[:custom]).to eq("-b 1500k -ab 160000 -f webm -g 30 -vf \"movie=path/to/file.png [logo]; [in][logo] overlay=5:main_h-overlay_h-5 [out]\"")
 
-          path.should == "video/path/tmpfile.#{format}"
+          expect(path).to eq("video/path/tmpfile.#{format}")
         end
 
         converter.encode_video(format, watermark: {
@@ -193,14 +193,14 @@ describe CarrierWave::Video do
       end
 
       it "only requires path watermark parameter" do
-        movie.should_receive(:transcode) do |path, opts, codec_opts|
-          codec_opts.should == {preserve_aspect_ratio: :width}
+        expect(movie).to receive(:transcode) do |path, opts, codec_opts|
+          expect(codec_opts).to eq({preserve_aspect_ratio: :width})
 
-          opts[:video_codec].should == 'libvpx'
-          opts[:audio_codec].should == 'libvorbis'
-          opts[:custom].should == "-b 1500k -ab 160000 -f webm -g 30 -vf \"movie=path/to/file.png [logo]; [in][logo] overlay= [out]\""
+          expect(opts[:video_codec]).to eq('libvpx')
+          expect(opts[:audio_codec]).to eq('libvorbis')
+          expect(opts[:custom]).to eq("-b 1500k -ab 160000 -f webm -g 30 -vf \"movie=path/to/file.png [logo]; [in][logo] overlay= [out]\"")
 
-          path.should == "video/path/tmpfile.#{format}"
+          expect(path).to eq("video/path/tmpfile.#{format}")
         end
 
         converter.encode_video(format, watermark: {
@@ -209,8 +209,8 @@ describe CarrierWave::Video do
       end
 
       it "removes watermark options from common options" do
-        movie.should_receive(:transcode) do |path, opts, codec_opts|
-          opts.should_not have_key(:watermark)
+        expect(movie).to receive(:transcode) do |path, opts, codec_opts|
+          expect(opts).not_to have_key(:watermark)
         end
 
         converter.encode_video(format, watermark: {
@@ -223,13 +223,13 @@ describe CarrierWave::Video do
 
     context "with resolution set to :same" do
       before do
-        File.should_receive(:rename)
-        movie.stub(:resolution).and_return('1280x720')
+        expect(File).to receive(:rename)
+        allow(movie).to receive(:resolution).and_return('1280x720')
       end
 
       it "sets the output resolution to match that of the input" do
-        movie.should_receive(:transcode) do |path, opts, codec_opts|
-          opts[:resolution].should == '1280x720'
+        expect(movie).to receive(:transcode) do |path, opts, codec_opts|
+          expect(opts[:resolution]).to eq('1280x720')
         end
 
         converter.encode_video(format, resolution: :same)
@@ -238,20 +238,20 @@ describe CarrierWave::Video do
 
     context "with custom passed in" do
       before do
-        File.should_receive(:rename)
+        expect(File).to receive(:rename)
       end
 
       it "takes the provided custom param" do
-        movie.should_receive(:transcode) do |path, opts, codec_opts|
-          opts[:custom].should == '-preset slow' # a la changes in ffmpeg 0.11.1
+        expect(movie).to receive(:transcode) do |path, opts, codec_opts|
+          expect(opts[:custom]).to eq('-preset slow') # a la changes in ffmpeg 0.11.1
         end
 
         converter.encode_video(format, custom: '-preset slow')
       end
 
       it "maintains the watermark params" do
-        movie.should_receive(:transcode) do |path, opts, codec_opts|
-          opts[:custom].should == "-preset slow -vf \"movie=path/to/file.png [logo]; [in][logo] overlay= [out]\""
+        expect(movie).to receive(:transcode) do |path, opts, codec_opts|
+          expect(opts[:custom]).to eq("-preset slow -vf \"movie=path/to/file.png [logo]; [in][logo] overlay= [out]\"")
         end
 
         converter.encode_video(format, custom: '-preset slow', watermark: {
@@ -266,14 +266,14 @@ describe CarrierWave::Video do
       let(:params) { { resolution: "640x360", watermark: {}, video_codec: "libvpx", audio_codec: "libvorbis", custom: "-b 1500k -ab 160000 -f webm -g 30" } }
 
       before do
-        File.should_receive(:rename)
-        movie.stub(:resolution).and_return('1280x720')
+        expect(File).to receive(:rename)
+        allow(movie).to receive(:resolution).and_return('1280x720')
       end
 
       it "calls the block, with the movie file and params" do
-        movie.should_receive(:transcode) do |path, format_opts, codec_opts|
-          format_opts[:video_codec].should == 'libvpx'
-          format_opts[:audio_codec].should == 'libvorbis'
+        expect(movie).to receive(:transcode) do |path, format_opts, codec_opts|
+          expect(format_opts[:video_codec]).to eq('libvpx')
+          expect(format_opts[:audio_codec]).to eq('libvorbis')
         end
 
         expect {
@@ -284,8 +284,8 @@ describe CarrierWave::Video do
       it "allows the block to modify the params" do
         block = Proc.new { |input, params| params[:custom] = '-preset slow' }
 
-        movie.should_receive(:transcode) do |path, format_opts, codec_opts|
-          format_opts[:custom].should == '-preset slow'
+        expect(movie).to receive(:transcode) do |path, format_opts, codec_opts|
+          expect(format_opts[:custom]).to eq('-preset slow')
         end
 
         converter.encode_video(format, opts, &block)
@@ -297,8 +297,8 @@ describe CarrierWave::Video do
           params[:watermark][:path] = 'customized/path'
         end
 
-        movie.should_receive(:transcode) do |path, format_opts, codec_opts|
-          format_opts[:custom].should == '-preset slow -vf "movie=customized/path [logo]; [in][logo] overlay= [out]"'
+        expect(movie).to receive(:transcode) do |path, format_opts, codec_opts|
+          expect(format_opts[:custom]).to eq('-preset slow -vf "movie=customized/path [logo]; [in][logo] overlay= [out]"')
         end
 
         converter.encode_video(format, opts, &block)
@@ -311,8 +311,8 @@ describe CarrierWave::Video do
           params[:resolution] = '1x1'
         end
 
-        movie.should_receive(:transcode) do |path, format_opts, codec_opts|
-          format_opts[:resolution].should == '1x1'
+        expect(movie).to receive(:transcode) do |path, format_opts, codec_opts|
+          expect(format_opts[:resolution]).to eq('1x1')
         end
 
         converter.encode_video(format, opts, &block)
@@ -328,16 +328,16 @@ describe CarrierWave::Video do
 
 
     before do
-      converter.model.stub(:logger).and_return(logger)
-      File.should_receive(:rename)
-      converter.stub(:current_path).and_return('video/path/input.mov')
+      allow(converter.model).to receive(:logger).and_return(logger)
+      expect(File).to receive(:rename)
+      allow(converter).to receive(:current_path).and_return('video/path/input.mov')
     end
 
     context "no options set" do
       it "calls transcode with correct format options" do
         transcoder = double(:transcoder)
-        CarrierWave::Video::FfmpegTheora.should_receive(:new).with(movie_path, output_path).and_return(transcoder)
-        transcoder.should_receive(:run)
+        expect(CarrierWave::Video::FfmpegTheora).to receive(:new).with(movie_path, output_path).and_return(transcoder)
+        expect(transcoder).to receive(:run)
 
         converter.encode_ogv({})
       end
@@ -345,13 +345,13 @@ describe CarrierWave::Video do
 
     context "with logger set" do
       before do
-        converter.model.stub(:logger).and_return(logger)
+        allow(converter.model).to receive(:logger).and_return(logger)
       end
 
       it "calls transcode with correct format options and passes logger to transcoder" do
         transcoder = double(:transcoder)
-        CarrierWave::Video::FfmpegTheora.should_receive(:new).with(movie_path, output_path).and_return(transcoder)
-        transcoder.should_receive(:run).with(logger)
+        expect(CarrierWave::Video::FfmpegTheora).to receive(:new).with(movie_path, output_path).and_return(transcoder)
+        expect(transcoder).to receive(:run).with(logger)
 
         converter.encode_ogv({logger: :logger})
       end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,6 @@ require 'streamio-ffmpeg'
 require 'carrierwave/video'
 
 RSpec.configure do |config|
+  config.raise_errors_for_deprecations!
   config.mock_with :rspec
 end
